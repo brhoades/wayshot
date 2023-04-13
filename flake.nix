07100af227ab806fcf58ed967f93356f591fb372
Billy J Rhoades II <me@brod.es>
2023-04-13 07:54:26 -0700

    add envrc/flake

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "advent-of-code flake with shell";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }: let
+    pkgsFor = system: import nixpkgs {
+      inherit system;
+    }; in (flake-utils.lib.eachDefaultSystem (system: {
+      # envrc
+      devShells.default = with (pkgsFor system); mkShell {
+        buildInputs = [
+          cargo
+          rustc
+          rust-analyzer
+          clippy
+          pkg-config
+          dbus.dev
+          xorg.libxcb
+          xorg.libXrandr
+        ];
+      };
+    }));
+}
